07df26dbcd05379554783bf42e31309cc8926bd5
Cristina-Lue <aracelylue5@gmail.com>
2024-07-21 20:11:00 -0600

    Add files via upload

---
+++ CuadroDialogo.java
@@ -0,0 +1,11 @@
+package demoJoptionPane;
+import javax.swing.JOptionPane;
+
+public class CuadroDialogo {
+    public static void main(String[] args) {
+        //invoca el metodo showMessageDialog de la clase JOptionPane
+        JOptionPane.showMessageDialog(null,
+                "Bienvenido a IntelliJ IDEA IDE");
+        //System.exit(0);
+    }
+}
