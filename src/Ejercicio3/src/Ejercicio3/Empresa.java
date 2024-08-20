5914855c4ff49fd0e59718366cc206ae2cf17694
PL240092 <aracelylue5@gmail.com>
2024-08-21 01:56:43 +0200

    Agregando Clase desarrollador

--- src/Ejercicio3/src/Ejercicio3/Empresa.java
+++ src/Ejercicio3/src/Ejercicio3/Empresa.java
@@ -4,14 +4,18 @@ public class Empresa {
         public static void main(String[] args) {
             Empleado emp1 = new Empleado("Stefany Carbajal", 3000);
             Gerente ger1 = new Gerente("Lucia HErnandez", 5000, 2000);
-
+            Desarrollador dev1 = new Desarrollador("Aslly Hernandez", 1000, 500);
+                
             System.out.println("Información del Empleado:");
             emp1.mostrarInfo();
             System.out.println();
 
             System.out.println("Información del Gerente:");
             ger1.mostrarInfo();
-
+            System.out.println();
+                
             System.out.println("Información del desarrollador:");
+              dev1.mostrarInfo();
+                
         }
 }
