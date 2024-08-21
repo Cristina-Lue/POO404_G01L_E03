d1111d6e3d67d702f311ee84aed9fe1e1801a260
luciamile <74024261+luciamile@users.noreply.github.com>
2024-08-21 17:12:23 -0600

    paso6
    métodos restantes

--- SaludoPersonalizado/src/com/saludo/SaludoPersonalizado.java
+++ SaludoPersonalizado/src/com/saludo/SaludoPersonalizado.java
@@ -87,4 +87,60 @@ public class SaludoPersonalizado {
         return false;
     }
 
+    // Método para calcular la edad y saludar
+    public static void saludarYCalcularEdad(String nombreCompleto, String sexo, String fechaNacimiento) throws ParseException {
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("dd-MM-yyyy");
+        Date fechaNac = formatoFecha.parse(fechaNacimiento);
+
+        Calendar calendario = Calendar.getInstance();
+        calendario.setTime(fechaNac);
+
+        int anioNacimiento = calendario.get(Calendar.YEAR);
+        int anioActual = Calendar.getInstance().get(Calendar.YEAR);
+        int edad = anioActual - anioNacimiento;
+
+        String[] nombreDividido = nombreCompleto.split(" ");
+        String nombre = nombreDividido[0];
+        String apellido = nombreDividido.length > 1 ? nombreDividido[1] : "";
+
+        if (edad > 30) {
+            if (sexo.equalsIgnoreCase("Masculino")) {
+                System.out.println("Buenos días Mr. " + apellido);
+            } else if (sexo.equalsIgnoreCase("Femenino")) {
+                System.out.println("Buenos días Ms. " + apellido);
+            }
+        } else {
+            if (sexo.equalsIgnoreCase("Masculino")) {
+                System.out.println("Hola amigo " + nombre);
+            } else if (sexo.equalsIgnoreCase("Femenino")) {
+                System.out.println("Hola amiga " + nombre);
+            }
+        }
+
+        System.out.println("Tienes " + edad + " años.");
+    }
+
+    // Método para determinar si un año es bisiesto
+    public static boolean esBisiesto(int anio) {
+        return (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
+    }
+
+    // Método para obtener el año de la fecha de nacimiento
+    public static int obtenerAnio(String fechaNacimiento) {
+        return Integer.parseInt(fechaNacimiento.split("-")[2]);
+    }
+
+    // Método para mostrar el día de la semana de nacimiento
+    public static void mostrarDiaDeLaSemana(String fechaNacimiento) throws ParseException {
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("dd-MM-yyyy");
+        Date fecha = formatoFecha.parse(fechaNacimiento);
+
+        Calendar calendario = Calendar.getInstance();
+        calendario.setTime(fecha);
+
+        String[] diasSemana = {"domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"};
+        int diaSemana = calendario.get(Calendar.DAY_OF_WEEK) - 1;
+
+        System.out.println("Naciste un " + diasSemana[diaSemana]+".");
+    }
 }
